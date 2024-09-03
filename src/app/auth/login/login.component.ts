3c08c5e8a4b1ee858fb4f1a1c80f479fe5e72ae1
devmanoshri <manoshri.no@gmail.com>
2024-09-03 15:13:18 +0200

    form update

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,90 @@
+import { Component, DestroyRef, OnInit, inject } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { of, debounceTime } from 'rxjs';
+
+function mustContainHash(control: AbstractControl) {
+  if (control.value.includes('#')) {
+    return null;
+  }
+  return { doesNotContainsHash: true };
+}
+
+function emailIsUnique(control: AbstractControl) {
+  if (control.value !== 'test@test.com') {
+    return of(null);
+  }
+  return of({ notUnique: true });
+}
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+})
+export class LoginComponent implements OnInit {
+  private destroyRef = inject(DestroyRef);
+
+  form = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.email, Validators.required],
+      asyncValidators: [emailIsUnique],
+    }),
+    password: new FormControl('', {
+      validators: [
+        Validators.required,
+        Validators.minLength(6),
+        mustContainHash,
+      ],
+    }),
+  });
+
+  get emailIsInvalid() {
+    return (
+      this.form.controls.email.touched &&
+      this.form.controls.email.dirty &&
+      this.form.controls.email.invalid
+    );
+  }
+  get passwordIsInvalid() {
+    return (
+      this.form.controls.password.touched &&
+      this.form.controls.password.dirty &&
+      this.form.controls.password.invalid
+    );
+  }
+
+  ngOnInit() {
+    const savedForm = window.localStorage.getItem('saved-login-form');
+    if (savedForm) {
+      const loadedForm = JSON.parse(savedForm);
+      this.form.patchValue({
+        email: loadedForm.email,
+      });
+    }
+
+    const sub = this.form.valueChanges.pipe(debounceTime(500)).subscribe({
+      next: (value) => {
+        window.localStorage.setItem(
+          'saved-login-form',
+          JSON.stringify({ email: value.email })
+        );
+      },
+    });
+
+    this.destroyRef.onDestroy(() => sub.unsubscribe());
+  }
+
+  onSubmit() {
+    const enteredEmail = this.form.value.email;
+    const enteredPassword = this.form.value.password;
+    console.log(enteredEmail, enteredPassword);
+  }
+}
